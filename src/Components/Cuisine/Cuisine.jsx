f3ca35202a37a7664ebcb870d1b34d79977ac19e
Sahanee Arman <115354835+sarman12@users.noreply.github.com>
2024-07-21 00:35:24 +0530

    6th day

--- src/Components/Cuisine/Cuisine.jsx
+++ src/Components/Cuisine/Cuisine.jsx
@@ -1,34 +1,35 @@
-import React from 'react'
-import './Cuisine.css'
-import I01 from '../..//assets/I01.jpg'
-import I02 from '../..//assets/I02.jpg'
-import I03 from '../..//assets/I03.jpg'
-import I04 from '../..//assets/I04.jpg'
-import I05 from '../..//assets/I05.jpg'
-import I06 from '../..//assets/I06.jpg'
-import I07 from '../..//assets/I07.jpg'
-import I08 from '../..//assets/I08.jpg'
-import I09 from '../..//assets/I09.jpg'
-import I10 from '../..//assets/I010.jpg'
-import I11 from '../..//assets/I011.jpg'
-import I12 from '../..//assets/I012.jpg'
-import I13 from '../..//assets/I013.jpg'
-import I14 from '../..//assets/I014.jpg'
-import I15 from '../..//assets/I015.jpg'
-import I16 from '../..//assets/I016.jpg'
+import React from 'react';
+import './Cuisine.css';
+import I01 from '../..//assets/I01.jpg';
+import I02 from '../..//assets/I02.jpg';
+import I03 from '../..//assets/I03.jpg';
+import I04 from '../..//assets/I04.jpg';
+import I05 from '../..//assets/I05.jpg';
+import I06 from '../..//assets/I06.jpg';
+import I07 from '../..//assets/I07.jpg';
+import I08 from '../..//assets/I08.jpg';
+import I09 from '../..//assets/I09.jpg';
+import I10 from '../..//assets/I010.jpg';
+import I11 from '../..//assets/I011.jpg';
+import I12 from '../..//assets/I012.jpg';
+import I13 from '../..//assets/I013.jpg';
+import I14 from '../..//assets/I014.jpg';
+import I15 from '../..//assets/I015.jpg';
+import I16 from '../..//assets/I016.jpg';
 
-import C01 from '../..//assets/C01.jpg'
-import C02 from '../..//assets/C02.jpg'
-import C03 from '../..//assets/C03.jpg'
-import C04 from '../..//assets/C04.jpg'
-import C05 from '../..//assets/C05.jpg'
-import C06 from '../..//assets/C06.jpg'
+import C01 from '../..//assets/C01.jpg';
+import C02 from '../..//assets/C02.jpg';
+import C03 from '../..//assets/C03.jpg';
+import C04 from '../..//assets/C04.jpg';
+import C05 from '../..//assets/C05.jpg';
+import C06 from '../..//assets/C06.jpg';
 
 const cuisinesData = [
   {
     name: "Indian Cuisine",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas quo veritatis odit deleniti molestias quibusdam adipisci omnis modi quod labore. Dolores reprehenderit placeat facilis ducimus beatae fuga totam ipsam doloribus vero maiores.",
-    images: [I01, I02, I03, I04, I05, I06, I07, I08, I09, I10, I11, I12, I13, I14, I15, I16]
+    images: [I01, I02, I03, I04, I05, I06, I07, I08, I09, I10, I11, I12, I13, I14, I15, I16],
+    name_of_dishes: ["Litti Chokha(Bihar)","Poha Jalebi(MP)","Bikaneri Kachori(Rajasthan)","Akki Roti(Karnataka)","Paddu(TN)","Ghugni Chaat(WB)","Chole Bhaturey(Delhi)","Tunde Kabab(Lucknow)","Mirchi Bhaji(Hydrabad)","Fuchka(WB)","Kathi Rolls(WB)","Pav Bhaji(Mumbai)","Butter Chicken(Delhi)","Vada Pav(Mumbai)","Dal Makhani(Punjab)","Momos(Sikkim)","Aloo Gobhi(Punjab)","Rogan Josh(Kashmir)","Tandoori Chicken(Punjab)"]
   },
   {
     name: "Chinese Cuisine",
@@ -39,28 +40,23 @@ const cuisinesData = [
     name: "Italian Cuisine",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas quo veritatis odit deleniti molestias quibusdam adipisci omnis modi quod labore. Dolores reprehenderit placeat facilis ducimus beatae fuga totam ipsam doloribus vero maiores.",
     images: [I01, I02, I03, I04, I05, I06, I07, I08, I09, I10, I11, I12, I13, I14, I15, I16]
-
-  }
-  ,
+  },
   {
     name: "French Cuisine",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas quo veritatis odit deleniti molestias quibusdam adipisci omnis modi quod labore. Dolores reprehenderit placeat facilis ducimus beatae fuga totam ipsam doloribus vero maiores.",
     images: [I01, I02, I03, I04, I05, I06, I07, I08, I09, I10, I11, I12, I13, I14, I15, I16]
-
   },
   {
     name: "Mexican Cuisine",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas quo veritatis odit deleniti molestias quibusdam adipisci omnis modi quod labore. Dolores reprehenderit placeat facilis ducimus beatae fuga totam ipsam doloribus vero maiores.",
     images: [I01, I02, I03, I04, I05, I06, I07, I08, I09, I10, I11, I12, I13, I14, I15, I16]
-
   },
   {
     name: "Canadian Cuisine",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas quo veritatis odit deleniti molestias quibusdam adipisci omnis modi quod labore. Dolores reprehenderit placeat facilis ducimus beatae fuga totam ipsam doloribus vero maiores.",
     images: [I01, I02, I03, I04, I05, I06, I07, I08, I09, I10, I11, I12, I13, I14, I15, I16]
-
   }
-]
+];
 
 function Cuisine() {
   return (
@@ -78,7 +74,11 @@ function Cuisine() {
                     key={imgIndex}
                     className="cuisine_image"
                     style={{ backgroundImage: `url(${image})` }}
-                  />
+                  >
+                    {cuisine.name_of_dishes && cuisine.name_of_dishes[imgIndex] && (
+                      <p>{cuisine.name_of_dishes[imgIndex]}</p>
+                    )}
+                  </div>
                 ))}
               </div>
             </div>
@@ -86,7 +86,7 @@ function Cuisine() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Cuisine
+export default Cuisine;
